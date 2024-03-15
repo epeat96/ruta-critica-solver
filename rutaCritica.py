5e55a0d38254d73b801f143095cc4a6087c2e8e4
epeat96 <44922559+epeat96@users.noreply.github.com>
2024-03-15 15:42:30 -0300

    Agrega array de dependencias

--- rutaCritica.py
+++ rutaCritica.py
@@ -12,28 +12,48 @@ nombre_archivo = 'tabla.csv'
 
 class Tarea:
 
-    def __init__(self, nombre, duracion, dependencias, es, ef, ls, lf):
+    def __init__(self, nombre, duracion, es, ef, ls, lf):
         self.nombre = nombre
         self.duracion = duracion
-        self.dependencias = dependencias
         self.es = es
         self.ef = ef
         self.ls = ls
         self.lf = lf
+        self.dependencias = []
+        self.dependientes = []
 
     def __str__(self):
-        return f"nombre : {self.nombre} \n \t duracion : {self.duracion} \n \t dependencias : {self.dependencias} \n \t es : {self.es} \n \t ef : {self.ef} \n \t ls : {self.ls} \n \t lf : {self.lf}"
+        return f"nombre : {self.nombre} \n \t duracion : {self.duracion} \n \t es : {self.es} \n \t ef : {self.ef} \n \t ls : {self.ls} \n \t lf : {self.lf}"
+
+    def agregarDependencias(self,dependencias):
+        self.dependencias.extend(dependencias)
+
+    def agregarDependientes(self,dependientes):
+        self.dependientes.extend(dependientes)
 
 
 tareas = []
+dependencias = []
 rutas_criticas = []
 matriz_de_adyacencia = [[]]
 
 with open(nombre_archivo, mode='r', encoding='utf-8') as archivo:
     lector = csv.DictReader(archivo)
 
-    for fila in lector:
-        tareas.append(Tarea(fila["Tareas"], fila["Duraciones"], fila["Dependencias"].split(","), 0, 0, 0, 0))
-    print("Tareas:")
-    for tarea in tareas:
-        print(f"{tarea.nombre}: \n \t {tarea.__str__()}")
+    for indice, fila in enumerate(lector):
+        tareas.append(Tarea(fila["Tareas"], fila["Duraciones"], 0, 0, 0, 0))
+        dependencias.append(fila["Dependencias"])
+    for indice, tarea in enumerate(tareas):
+
+        if dependencias[indice] == "-":
+            continue
+
+        nombres_dependencias = set(dependencias[indice].split(','))
+
+        dependencias_a_agregar = [dependencia for dependencia in tareas if dependencia.nombre in nombres_dependencias]
+
+        tarea.agregarDependencias(dependencias_a_agregar)
+
+        print(f"Tarea: {tarea} \n Dependencias:")
+        for dependencia in dependencias_a_agregar:
+            print(dependencia)
