dd2a7fe7ecc4a29eaec1fbf9a104b3f231022d2a
epeat96 <44922559+epeat96@users.noreply.github.com>
2024-03-14 21:54:23 -0300

    Cambia enfoque a OOP con matriz de adyacencias

--- rutaCritica.py
+++ rutaCritica.py
@@ -9,27 +9,31 @@ import csv
 
 nombre_archivo = 'tabla.csv'
 
+
+class Tarea:
+
+    def __init__(self, nombre, duracion, dependencias, es, ef, ls, lf):
+        self.nombre = nombre
+        self.duracion = duracion
+        self.dependencias = dependencias
+        self.es = es
+        self.ef = ef
+        self.ls = ls
+        self.lf = lf
+
+    def __str__(self):
+        return f"nombre : {self.nombre} \n \t duracion : {self.duracion} \n \t dependencias : {self.dependencias} \n \t es : {self.es} \n \t ef : {self.ef} \n \t ls : {self.ls} \n \t lf : {self.lf}"
+
+
 tareas = []
-duraciones = []
-dependencias = []
-es = []
-ef = []
-fs = []
-fs = []
-holgura = []
 rutas_criticas = []
+matriz_de_adyacencia = [[]]
 
 with open(nombre_archivo, mode='r', encoding='utf-8') as archivo:
     lector = csv.DictReader(archivo)
 
     for fila in lector:
-        dependencias.append(fila["Dependencias"])
-        tareas.append(fila["Tareas"])
-        duraciones.append(fila["Duraciones"])
-
-    for tarea, indice in enumerate(tareas):
-
-
-    print(f"Dependencias: {dependencias}")
-    print(f"Tareas: {tareas}")
-    print(f"Duraciones: {duraciones}")
+        tareas.append(Tarea(fila["Tareas"], fila["Duraciones"], fila["Dependencias"].split(","), 0, 0, 0, 0))
+    print("Tareas:")
+    for tarea in tareas:
+        print(f"{tarea.nombre}: \n \t {tarea.__str__()}")
